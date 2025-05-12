df013a83739ec5b669de6cd11b060bcd15dcebea
Filipe Mota <filipera3@gmail.com>
2025-05-12 19:59:37 -0300

    feat: add QrCodeGeneratorService

--- src/main/java/com/ofilipecode/qrcode/generator/controller/QrCodeController.java
+++ src/main/java/com/ofilipecode/qrcode/generator/controller/QrCodeController.java
@@ -2,6 +2,7 @@ package com.ofilipecode.qrcode.generator.controller;
 
 import com.ofilipecode.qrcode.generator.dto.request.QrCodeGenerateRequest;
 import com.ofilipecode.qrcode.generator.dto.response.QrCodeGeneratorResponse;
+import com.ofilipecode.qrcode.generator.service.QrCodeGeneratorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/qrcode")
 public class QrCodeController {
 
+private final QrCodeGeneratorService qrCodeGeneratorService;
+
+    public QrCodeController(QrCodeGeneratorService qrCodeGeneratorService) {
+        this.qrCodeGeneratorService = qrCodeGeneratorService;
+    }
+
     @PostMapping
     public ResponseEntity<QrCodeGeneratorResponse> generate(@RequestBody QrCodeGenerateRequest request) {
+        try {
+            QrCodeGeneratorResponse response = this.qrCodeGeneratorService.upload(request.text());
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
 
-        return null;
     }
 }
---
+++ src/main/java/com/ofilipecode/qrcode/generator/service/QrCodeGeneratorService.java
@@ -0,0 +1,38 @@
+package com.ofilipecode.qrcode.generator.service;
+
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.ofilipecode.qrcode.generator.dto.request.QrCodeGenerateRequest;
+import com.ofilipecode.qrcode.generator.dto.response.QrCodeGeneratorResponse;
+import com.ofilipecode.qrcode.generator.ports.StoragePort;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+public class QrCodeGeneratorService {
+
+    private final StoragePort storage;
+
+    public QrCodeGeneratorService(StoragePort storage) {
+        this.storage = storage;
+    }
+
+    public QrCodeGeneratorResponse upload(String text) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, com.google.zxing.BarcodeFormat.QR_CODE, 200, 200);
+
+        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+        byte[] pngQrCodeData = pngOutputStream.toByteArray();
+
+        String url = storage.uploadFile(pngQrCodeData, UUID.randomUUID().toString(), "image/png");
+
+        return new QrCodeGeneratorResponse(url);
+
+    }
+}
